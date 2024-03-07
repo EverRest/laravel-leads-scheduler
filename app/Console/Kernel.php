ac9645d99226064933a8c89187aad2178431d8d8
EverRest <medynskyypavlo@gmail.com>
2024-03-07 14:42:52 +0100

    remove leads:send command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('leads:send')->everyMinute();
+//        $schedule->command('leads:send')->everyMinute();
 //        $schedule->command('lead:delete-proxy')->daily();
     }
 
