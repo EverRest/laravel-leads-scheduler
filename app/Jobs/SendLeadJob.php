756a0a891832fb8685442e3155dfe2c2a9f35fff
EverRest <medynskyypavlo@gmail.com>
2024-03-10 11:41:00 +0100

    debug

--- app/Jobs/SendLeadJob.php
+++ app/Jobs/SendLeadJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\LeadRedirect;
 use App\Services\Lead\LeadRedirectService;
 use App\Services\Partner\PartnerServiceFactory;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 class SendLeadJob extends LeadJob
 {
@@ -17,12 +18,13 @@ class SendLeadJob extends LeadJob
 
     /**
      * Execute the job.
+     * @throws FileNotFoundException
      */
     public function handle(LeadRedirectService $leadRedirectService): void
     {
         /** @var LeadRedirect $leadRedirect */
         $service = PartnerServiceFactory::createService($this->lead->partner->external_id);
         $service->send($this->lead);
-        $leadRedirectService->generateScreenshotByLeadRedirect($this->lead);
+//        $leadRedirectService->generateScreenshotByLeadRedirect($this->lead);
     }
 }
--- app/Repositories/LeadRepository.php
+++ app/Repositories/LeadRepository.php
@@ -62,8 +62,8 @@ final class LeadRepository extends Repository
     public function getSentLeads(): Collection
     {
         return $this->query()
-            ->where('scheduled_at', '<=', Carbon::now()->toDateTimeString())
-            ->where('scheduled_at', '>=', Carbon::now()->subMinutes(2)->toDateTimeString())
+            ->where('scheduled_at', '<', Carbon::now()->toDateTimeString())
+            ->where('scheduled_at', '>=', Carbon::now()->subMinutes(1)->toDateTimeString())
             ->get();
     }
 
