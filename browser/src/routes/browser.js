856cb9e1e886fdbb44194824445067628527e18e
EverRest <medynskyypavlo@gmail.com>
2024-03-11 08:13:13 +0100

    changed nodejs flow

--- browser/src/routes/browser.js
+++ browser/src/routes/browser.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const Browser = require('../components/browser')
 const router = Router()
+const fs = require('fs').promises; // Using promises version for async/await
+const path = require('path');
 
 router.get('/browser', (req, res, next) => {
     res.send({message: 'success'})
@@ -9,7 +11,10 @@ router.get('/browser', (req, res, next) => {
 router.post('/browser', async (req, res, next) => {
     const {proxy, url} = req.body
     try {
+        const { screenshot, filename } = await getScreenshot(proxy, url);
         const base64screenshot = await getScreenshot(proxy, url);
+        const savePath = path.join(__dirname, 'files', filename);
+        await fs.writeFile(savePath, screenshot);
         return res.send({status: 200, screenshot: base64screenshot, message: 'success'})
     }catch (e) {
         return res.send({status: 400, message: e.message, screenshot: ''})
